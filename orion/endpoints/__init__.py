9022a39fcb4f60144f6680523951e27fbd2b7c04
BruRdgz <bru.rdgz@gmailcom>
2025-06-19 23:12:46 -0300

    Consertar erro de importe circular

--- orion/endpoints/__init__.py
+++ orion/endpoints/__init__.py
@@ -3,17 +3,17 @@ import pkgutil
 
 from fastapi import APIRouter
 
-import orion
+from orion import endpoints  # noqa: F401
 
 
 def discover_routers() -> list[APIRouter]:
     routers: list[APIRouter] = []
 
-    for _finder, module_name, _is_pkg in pkgutil.iter_modules(orion.endpoints.__path__):  # type: ignore
+    for _finder, module_name, _is_pkg in pkgutil.iter_modules(endpoints.__path__):  # type: ignore
         if module_name.startswith("_"):
             continue
 
-        full_module_name = f"{orion.endpoints.__name__}.{module_name}"  # type: ignore
+        full_module_name = f"{endpoints.__name__}.{module_name}"  # type: ignore
         module = importlib.import_module(full_module_name)
 
         router = getattr(module, "router", None)
